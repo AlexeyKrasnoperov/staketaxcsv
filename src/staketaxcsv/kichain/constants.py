3ac41a1b9762346bbf2beecf6aaa577f2b1dd1cb
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2024-11-24 23:44:53 +0100

    Support KiChain

---
+++ src/staketaxcsv/kichain/constants.py
@@ -0,0 +1,1 @@
+MINTSCAN_LABEL_KICHAIN = "ki-chain"
---
+++ src/staketaxcsv/kichain/progress_kichain.py
@@ -0,0 +1,13 @@
+from staketaxcsv.kichain.config_kichain import localconfig
+from staketaxcsv.common.progress import Progress
+
+SECONDS_PER_PAGE = 4
+
+
+class ProgressKichain(Progress):
+
+    def __init__(self):
+        super().__init__(localconfig)
+
+    def set_estimate(self, count_pages):
+        self.add_stage("default", count_pages, SECONDS_PER_PAGE)
